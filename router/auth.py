bc5c98c58baea28abac06c090206156a419b5204
Hamitcan MALKOÇ <hamitcanmalkoc@appyfany.com>
2026-07-16 12:13:56 +0300

    refresh token

--- router/auth.py
+++ router/auth.py
@@ -8,6 +8,7 @@ from fastapi import (
 )
 from sqlmodel import select
 
+from core import secret
 from core.secret import JWT_SECRET_KEY
 from core.session import PGSessionDep
 from model.user import (
@@ -34,7 +35,6 @@ async def authenticate(user: User, session: PGSessionDep) -> dict:
     access = jwt.encode(
         {
             'user': db_user.id,
-            'sub': username,
             'role': 'user',
             'type': 'access',
             'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
@@ -45,7 +45,6 @@ async def authenticate(user: User, session: PGSessionDep) -> dict:
     ref = jwt.encode(
         {
             'user': db_user.id,
-            'sub': username,
             'role': 'user',
             'type': 'refresh',
             'exp': datetime.datetime.utcnow() + datetime.timedelta(days=30),
@@ -66,8 +65,46 @@ async def authenticate(user: User, session: PGSessionDep) -> dict:
 
 
 @router.post("/refresh")
-async def refresh(user: User, session: PGSessionDep) -> dict:
+async def refresh(ref: UserRefreshToken, session: PGSessionDep) -> dict:
+    payload = jwt.decode(ref.refresh_token, key=secret.JWT_SECRET_KEY, algorithms=['HS256'])
+    if payload['type'] not in ('refresh',):
+        raise ValueError('this is not a refresh token')
+
+    user_id = payload['user']
+
+    db_token = session.exec(select(UserRefreshToken).where(UserRefreshToken.user_id == user_id)).first()
+    if not db_token:
+        raise ValueError('refresh token not found')
+    if db_token.refresh_token != ref.refresh_token:
+        raise ValueError('refresh token not found')
+
+    access = jwt.encode(
+        {
+            'user': user_id,
+            'role': 'user',
+            'type': 'access',
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+            'iat': datetime.datetime.utcnow()
+        }, JWT_SECRET_KEY, algorithm="HS256"
+    )
+
+    ref = jwt.encode(
+        {
+            'user': user_id,
+            'role': 'user',
+            'type': 'refresh',
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=30),
+            'iat': datetime.datetime.utcnow()
+        }, JWT_SECRET_KEY, algorithm="HS256"
+    )
+
+    refresh_token: UserRefreshToken = UserRefreshToken(user_id=user_id, refresh_token=ref)
+
+    session.add(refresh_token)
+    session.commit()
+    session.refresh(refresh_token)
+
     return {
-        'access': '',
-        'refresh': ''
+        'access': access,
+        'refresh': ref
     }
